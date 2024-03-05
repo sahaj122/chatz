ce35d4d17881813e84db6b70aa43af3a735e2ccb
SAHAJJIT SINGH <sahajjitsingh@gmail.com>
2024-03-05 18:37:26 +0530

    updating index.js in server

--- server/index.js
+++ server/index.js
@@ -10,20 +10,29 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connetion Successfull");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
 
+// connecting database
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cx5ccid.mongodb.net/${process.env.DB_NAME}`, {
+    useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => console.log('Database Connected'))
+    .catch((err) => console.log(err))
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// mongoose
+//   .connect(process.env.MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("DB Connetion Successfull");
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/messages", messageRoutes);
 
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
