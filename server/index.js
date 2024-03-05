01c8e927c710710a288b8a6bfba0ed2c3987da16
SAHAJJIT SINGH <sahajjitsingh@gmail.com>
2024-03-06 03:59:24 +0530

    updating string

--- server/index.js
+++ server/index.js
@@ -9,50 +9,49 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+const uri = "mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kfwmszy.mongodb.net/?retryWrites=true&w=majority&appName=cluster0";
+
 
 // connecting database
 // mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kfwmszy.mongodb.net/${process.env.DB_NAME}`, {
-
-// connecting database
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kfwmszy.mongodb.net/${process.env.DB_NAME}`, {
-    useUnifiedTopology: true, useNewUrlParser: true })
-    .then(() => console.log('Database Connected'))
-    .catch((err) => console.log(err))
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-
-// mongoose
-//   .connect(process.env.MONGO_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("DB Connetion Successfull");
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//   });
-
+//     useUnifiedTopology: true, useNewUrlParser: true })
+//     .then(() => console.log('Database Connected'))
+//     .catch((err) => console.log(err))
 // app.use("/api/auth", authRoutes);
 // app.use("/api/messages", messageRoutes);
+
+mongoose
+  .connect(`"mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kfwmszy.mongodb.net/?retryWrites=true&w=majority&appName=cluster0";`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
 
-const io = new socket(server, {
-  cors: {
-      origin: `${process.env.CLIENT_URL}`,
-      methods: ['GET', 'POST']
-  }
-})
-
-// const io = socket(server, {
+// const io = new socket(server, {
 //   cors: {
-//     origin: "https://chatz-seven.vercel.app",
-//     credentials: true,
-//   },
-// });
+//       origin: `${process.env.CLIENT_URL}`,
+//       methods: ['GET', 'POST']
+//   }
+// })
+
+const io = socket(server, {
+  cors: {
+    origin: "https://chatz-seven.vercel.app",
+    credentials: true,
+  },
+});
 
 global.onlineUsers = new Map();
 io.on("connection", (socket) => {
